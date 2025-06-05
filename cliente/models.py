3876428e70cff15200d6e79f71315610eee4b80d
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-06-04 21:12:28 -0300

    agrega modelo Pais y campos nacimiento y pais_origen al modelo Cliente

--- cliente/models.py
+++ cliente/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.ForeignKey(
+        Pais, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self) -> str:
         return f"{self.apellido.upper()}, {self.nombre}"
