6499bd0b09a8703162be8a16161332105b4be7cc
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-06-18 21:33:11 -0300

    agrega funcionalidad de cierre de sesión con plantilla y botón en la barra de navegación

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", LoginView.as_view(template_name="core/login.html"), name="login"),
+    path("logout", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
 ]
