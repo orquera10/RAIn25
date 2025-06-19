9c067681c08ab7325cd9694d939918cde79eb44b
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-06-18 22:24:01 -0300

    agrega funcionalidad de registro de usuario con vista y plantilla; actualiza URLs y vistas correspondientes

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import UserForm
 
 
 def index(request):
     return render(request, "core/index.html")
+
+
+class UserCreateView(CreateView):
+    form_class = UserForm
+    template_name = "core/register.html"
+    success_url = reverse_lazy("core:login")
